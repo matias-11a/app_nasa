673395583b7a1197645eec63dc2b7a7186143497
mati-11a <matiasmardones@epet12smandes.edu.ar>
2024-06-14 02:40:20 -0300

    proyecto

---
+++ App.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+import Layout from "./src/screens/layout";
+import Router from "./src/routers/router";
+
+export default App = () => {
+  return <Router></Router>;
+};
